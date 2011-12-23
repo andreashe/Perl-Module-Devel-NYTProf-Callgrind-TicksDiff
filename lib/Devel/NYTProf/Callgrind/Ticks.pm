194bdbd44fae22e74519a1576b91f5de923c8244
ahernit <ahernit@cpan.org>
2011-12-23 18:36:51 +0100

    first

---
+++ lib/Devel/NYTProf/Callgrind/Ticks.pm
@@ -0,0 +1,239 @@
+package Devel::NYTProf::Callgrind::Ticks; # represents a mesh of Ticks read from a callgrind file
+
+use strict;
+use warnings;
+our $VERSION = '0.01';
+
+use Data::Dumper;
+
+use Moose;
+
+
+
+
+# file to be loaded
+has 'file' => (
+    is => 'rw',
+    isa => 'Str',
+);
+
+# returns the list of blocks,
+# or writes them
+has 'list' => (
+    is => 'rw',
+    isa => 'ArrayRef',
+    default => sub{[]},
+);
+
+
+has 'blocks_by_id' => (
+    is => 'rw',
+    isa => 'HashRef',
+    default => sub{{}},
+);
+
+
+
+
+sub BUILD{
+    my $self = shift;
+
+    # file to be loaded?
+    if ( $self->file() ){
+        $self->loadFile( $self->file() );
+    }
+
+}
+
+# loads the callgrind file into memory and starts linking
+sub loadFile{
+    my $self = shift;
+    my $file = shift;
+    
+    if ( not -f $file ){ die "file $file does not exist" }; 
+
+    open( my $fh, $file );
+
+    my $area;
+    my $block;
+    my @list;
+    while ( my $line = <$fh> ){
+        $line =~ s/\n//; # remove return
+
+        # starting an area
+        if ( $line =~ m/events:\s(\w+)/i ){
+            $area = lc($1);
+            next;
+        }
+
+        # skip line if not type ticks
+        if ( $area ne 'ticks' ){ next };
+
+        # empty line is cleaning the block buffer
+        if ( $line =~ m/^\s*$/ ){
+
+            # save the found block infos
+            if ( scalar( keys %$block ) != 0 ){ push @list,$block };
+
+            $block={};
+            next;
+        }
+
+        # is there a equals char? (=)
+        if ( $line =~ m/(\w+)=(.+)/ ){
+            my $key   = $1;
+            my $value = $2;
+            $block->{ $key } = $value;
+
+            if ( $key eq 'calls' ){
+                my ($count, $dstpos) = split(/ /, $value, 2);
+                $block->{ 'count' }  = $count;
+                $block->{ 'dstpos' } = $dstpos;
+            }
+        }
+
+        ## read the ticks
+        if ( $line =~ m/^(\d+) (\d+)$/ ){
+            $block->{ 'srcpos' } = $1;
+            $block->{ 'ticks' }  = $2;
+        }
+
+    } # while
+
+    # no blank line on the end? save the block if needed
+    if ( keys %$block ){ push @list,$block };
+
+    #print Dumper( \@list );
+
+    # save the callgrind list holding blocks
+    $self->list( \@list );
+    $self->_buildIdHash();
+
+    close( $fh );
+}
+
+
+# build hash to for list_by_id to find
+# nodes by a fingerprint/id
+sub _buildIdHash{
+    my $self = shift;
+    my $list = $self->list();
+    my $idhash = {};
+
+    foreach my $block (@$list){
+        my $id = $self->_createFingerprintOfBlock( $block );
+
+        $idhash->{ $id } = $block;
+    }
+    
+    #print Dumper( $idhash );
+    $self->blocks_by_id( $idhash );
+}
+
+# Adds a block. For example you start with an
+# empty object and wants to add blocks from
+# a different object. It will replace an existing
+# block if the definition existists already. So
+# addBlock can also be used to update a block.
+# If you update an existing block, it does break
+# the reference to the given hashref, it makes a copy
+# of the values.
+sub addBlock{
+    my $self = shift;
+    my $block = shift;
+
+    my $id = $self->_createFingerprintOfBlock( $block );
+    my $found = $self->blocks_by_id()->{ $id };
+
+    # if already in, replace it, otherwise add it
+    if ( $found ){
+        %{ $found } = %{ $block };
+    }else{
+        if ( scalar( keys %$block ) != 0 ){ 
+            push @{ $self->list() }, $block;
+            $self->blocks_by_id()->{ $id } = $block;
+        }
+    }
+
+}
+
+
+# Takes a callgrind block and creates a unique string
+# to compare different files and find the same block.
+sub _createFingerprintOfBlock{
+    my $self = shift;
+    my $block = shift;
+    my $id;
+
+    my @keys = qw( fl fn srcpos cfl cfn dstpos );
+
+    my @id;
+    foreach my $w (@keys){
+        push @id, $block->{ $w } || '';
+    }
+    $id = join("#", @id);
+
+    return $id;
+}
+
+
+# returns the equivalent block in that object to a given
+# strange block (from a different object).
+# Returns undef if not found.
+sub getBlockEquivalent{
+    my $self = shift;
+    my $block = shift;
+    my $found = undef;
+
+    my $id = $self->_createFingerprintOfBlock( $block );
+
+    if ( exists $self->blocks_by_id()->{ $id } ){
+        $found = $self->blocks_by_id()->{ $id };
+    }
+
+    return $found;
+}
+
+
+# Is the same as list()
+sub getBlocksAsArray{
+    my $self = shift;
+
+    return $self->list();
+}
+
+
+# Save the data to a callgrind file. The event type will
+# be 'Ticks', nothing else.
+sub saveFile{
+    my $self = shift;
+    my $file = shift;
+    
+
+
+    open( my $fh, ">$file" ) or die "Can not write file $file";
+
+        print $fh "events: Ticks\n\n";
+        my @pairs = qw( fl fn cfl cfn calls );
+
+        foreach my $node ( @{ $self->list() } ){
+
+            my @block = ();
+
+            foreach my $p ( @pairs ){
+                push @block,"$p=".$node->{$p} if exists $node->{$p};
+            }
+
+            print $fh join( "\n", @block )."\n";
+            print $fh $node->{'srcpos'}.' '.$node->{'ticks'}."\n";
+            print $fh "\n";
+
+
+        }
+        
+
+    close( $fh );
+
+}
+
+1;
---
+++ lib/Devel/NYTProf/Callgrind/TicksDiff.pm
@@ -0,0 +1,223 @@
+package Devel::NYTProf::Callgrind::TicksDiff; # Calculates a delta between 2 callgrind files
+
+use strict;
+use warnings;
+use Devel::NYTProf::Callgrind::Ticks;
+our $VERSION = '0.01';
+
+
+use Moose;
+
+# callgrind files to be compared
+has 'files' => (
+    is => 'rw',
+    isa => 'ArrayRef',
+    default => sub {[]},
+);
+
+
+has 'file_out' => (
+    is => 'rw',
+    isa => 'ArrayRef',
+    default => sub {[]},
+);
+
+# Objects of ticks Devel::NYTProf::Callgrind::TicksD
+has 'ticks_objects' => (
+    is => 'rw',
+    isa => 'ArrayRef',
+    builder => '_loadFiles',
+);
+
+
+has 'ticks_object_out' => (
+    is => 'rw',
+    default => undef,
+);
+
+# enable normalization
+has 'normalize' => (
+    is => 'rw',
+    isa => 'Bool',
+    default => 0,
+);
+
+
+# if negative ticks are allowed or be truncated to 0
+has 'allow_negative' => (
+    is => 'rw',
+    isa => 'Bool',
+    default => 0,
+);
+
+
+sub _loadFiles{
+    my $self = shift;
+    my @files = @{ $self->files() };
+    my @objs;
+
+    foreach my $file (@files){
+        
+        my $ticks = Devel::NYTProf::Callgrind::Ticks->new( file => $file );
+        push @objs, $ticks;
+    }
+
+    
+  $self->ticks_objects( \@objs );
+}
+
+
+
+# starts the compare process. So far it compares only
+# two files. Returning infos in a hash.
+sub compare{
+    my $self = shift;
+    my $objs = $self->ticks_objects(); 
+    my $result = {};
+
+    my $obj_a = $objs->[0];
+    my $obj_b = $objs->[1];
+
+    my $notfound = 0;
+    my $delta_total = 0;
+    my $delta_less = 0;
+    my $delta_more = 0;
+    my $max_less = 0;
+
+
+    ## remember deltas for new blocks
+    my $deltaInfo = [];
+
+    foreach my $block_a ( @{ $obj_a->list() } ){
+
+       my $block_b = $obj_b->getBlockEquivalent( $block_a );
+
+       if ( $block_b ){
+            my $delta = $self->diffBlocks( $block_a, $block_b );
+            $delta_total += $delta;
+            
+            if ( $delta > 0 ){
+                $delta_more += $delta;
+            }else{
+                $delta_less += $delta;
+
+                # remember the biggest negative value.
+                # to enable shifting when normalize is on
+                if ( $delta < $max_less ){
+                    $max_less = $delta;
+                }
+            }
+
+            push @$deltaInfo, {
+                            delta   => $delta,
+                            block_a => $block_a,
+                            block_b => $block_b,
+                              };
+
+
+            #print $delta."\n";
+
+       }else{
+            $notfound++;
+       }
+
+    }
+
+
+
+    ## build new delta blocks.
+    ## iterate over the stored delta info list with
+    ## refs to the original blocks
+    
+    ## new ticks object to store the delta info in
+    my $nobj =  Devel::NYTProf::Callgrind::Ticks->new();
+    my $norm = $self->normalize();
+    my $allow_negative = $self->allow_negative(); 
+    foreach my $deltaInfo ( @{ $deltaInfo } ){
+
+            my $block_a = $deltaInfo->{'block_a'};
+
+            ## now build a new block
+            my $nblock = {};
+            %{ $nblock } = %{ $block_a }; # copy the existing block
+
+            if ( scalar( keys %$nblock ) == 0 ){ next }; # skip empty
+
+            my $nticks = $deltaInfo->{'delta'}; # using the delta as ticks
+            
+            # normalization?
+            # It will shift up all values by the maximum nagative delta
+            # to have the lowest value as 0.
+            if ( $norm ){
+                $nticks = $nticks - $max_less; # it is a negative value
+            }
+
+            ## do not allow negative deltas.
+            ## to avoid wrong info, you may use normalize
+            if ( ($nticks < 0) && (!$allow_negative)){
+                $nticks = 0;
+            }
+
+            $nblock->{'ticks'} = $nticks;
+            
+            # store to the new ticks object
+            $nobj->addBlock( $nblock );        
+    }
+    
+    ## save to official location
+    $self->ticks_object_out( $nobj );
+
+
+    $result = {
+                not_found   => $notfound,
+                delta_more  => $delta_more,
+                delta_less  => $delta_less,
+                delta_total => $delta_total,
+                max_less    => $max_less,
+              };
+
+
+
+
+    return $result;
+}
+
+
+# Compares two single blocks (HasRefs) provided
+# by the Ticks class of this package. It returns
+# the tick difference between B and A. Means
+# B-Ticks - A-Ticks.
+sub diffBlocks{
+    my $self = shift;
+    my $blocka = shift or die "block as hashref required";
+    my $blockb = shift or die "block as hashref required";
+    
+    my $ta = $blocka->{'ticks'};
+    my $tb = $blockb->{'ticks'};
+
+    return $tb - $ta;
+}
+
+
+# just a wrapper around ticks_object_out
+sub getDeltaTicksObject{
+    my $self = shift;
+
+    return $self->ticks_object_out();
+}
+
+
+sub saveDiffFile{
+    my $self = shift;
+    my $file = shift or die "need filename";
+
+    my $obj = $self->ticks_object_out();
+#use Data::Dumper;
+#print Dumper($obj);exit;
+    $obj->saveFile( $file );
+
+    if ( ! -f $file ){ die "Did not create file $file" };
+
+}
+
+1;
